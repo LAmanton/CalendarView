a6657bc95aa915d4243bef3eb344163299ffe4f9
amantonl <laurent.amanton@gmail.com>
2023-03-06 14:52:56 +0100

    modif01

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView tv;
+CalendarView cv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textview);
+        cv=findViewById(R.id.calendarView);
+
+        // Récupérer le CalendarView depuis le layout
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+// Définir un listener pour le changement de date
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // Créer un objet Calendar pour la date sélectionnée
+                Calendar selectedDate = Calendar.getInstance();
+                selectedDate.set(Calendar.YEAR, year);
+                selectedDate.set(Calendar.MONTH, month);
+                selectedDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                // Afficher la date sélectionnée dans le format souhaité
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+                String selectedDateString = dateFormat.format(selectedDate.getTime());
+                tv.setText(selectedDateString);
+            }
+        });
     }
 }
